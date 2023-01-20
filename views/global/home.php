35b361cb72c21dd9beea7a7a39f37cffa094aef6
RenauxLea <lea.renaux@mail-ecv.fr>
2023-01-20 17:21:45 +0100

    add responsive

--- views/global/home.php
+++ views/global/home.php
@@ -3,17 +3,17 @@
     <h1 >Bienvenue sur la Terre du Milieu ! </h1>
 
     <p>Découvrez le monde fantastique du Seigneur des Anneaux avec les aventures de Frodon Sacquet dans 
-        cet univers aux paysages plus beaux les uns que les autres. </p>
-
-       
+        cet univers aux paysages plus beaux les uns que les autres. 
+    </p>
+  
     <div class="landscapes">
         <img class="landscape" src="public/assets/lord-of-the-rings-landscape-nature.jpg" >
-        <img class="landscape2" src="public/assets/landscape2.jpg" >
-        
+        <img class="landscape2" src="public/assets/landscape2.jpg" >    
     </div> 
 
     <h2>Synopsis: </h2>
-    <p>L'histoire commence lorsque, sur la Terre du Milieu, 
+    <p>
+        L'histoire commence lorsque, sur la Terre du Milieu, 
         le magicien Gandalf le Gris (Ian McKellen) découvre que l'anneau que possède le jeune Hobbit 
         Frodon Sacquet (Elijah Wood), qui lui a été légué par son oncle Bilbon et qui permet de se rendre 
         invisible, est en réalité l'Anneau unique recherché avidement par le seigneur ténébreux Sauron car 
@@ -22,17 +22,16 @@
         de le détruire. 
     </p>
 
-
     <h2> Réalisation du film: </h2>
 
+    <p>
+        Le Seigneur des anneaux est une trilogie cinématographique américano-néo-zélandaise de fantasy 
+        réalisée par Peter Jackson et fondée sur le roman du même nom en trois volumes de J. R. R. Tolkien. 
+    </p>
+    
     <div class="realisateurs">
         <img class="tolkien" src="public/assets/tolkien.jpg" >
         <img class="jackson" src="public/assets/jackson.jpg" >
-
     </div> 
-    <p>Le Seigneur des anneaux est une trilogie cinématographique américano-néo-zélandaise de fantasy 
-        réalisée par Peter Jackson et fondée sur le roman du même nom en trois volumes de J. R. R. Tolkien. </p>
-
-   
 
 </div>
