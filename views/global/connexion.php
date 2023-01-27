6274ff127cd392cc3aac2a1b263b1bf4150fb004
RenauxLea <lea.renaux@mail-ecv.fr>
2023-01-27 10:43:32 +0100

    add connexion page style

--- views/global/connexion.php
+++ views/global/connexion.php
@@ -1,12 +1,27 @@
-<form action="check_connexion" method="post">
-    <div>
-        <label for="name">Nom :</label>
-        <input type="text" id="name" name="username">
+<div class="connexion">
+    <div class="containerIllustrationConnexion">
+        <img src="public/assets/img/movie/lorPicture1.jpg" class="illustrationConnexion"/>
     </div>
-    <div>
-        <label for="msg">Mot de passe :</label>
-        <input type="password" id="passsword" name="password"></input>
+
+    <div class="cardConnexion">
+        <form action="check_connexion" method="post">
+            <h1>Connexion </h1>
+
+            <div class="inputConnexionContainer">
+                <label for="name">Nom :</label>
+                <input type="text" id="name" name="username" placeholder="Quentin">
+            </div>
+
+            <div class="inputConnexionContainer">
+                <label for="msg">Mot de passe :</label>
+                <input type="password" id="passsword" name="password" placeholder="motdepasse1234"></input>
+                    
+                <input type="submit" value="Je me connecte" class="submitButton"  >
+            </div>           
+        </form>
+
+        <div class="registerLink">
+            <a href="register">créer un compte</a>
+        </div>
     </div>
-    <input type="submit">
-</form>
-<a href="register">créer un compte</a>
+</div>
