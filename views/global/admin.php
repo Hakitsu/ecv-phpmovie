3624922ac987100957d3e17448ccd63828dd9ed6
RenauxLea <lea.renaux@mail-ecv.fr>
2023-02-01 13:38:20 +0100

    add style

--- views/global/admin.php
+++ views/global/admin.php
@@ -1,6 +1,8 @@
-<div>
-    <a href="add_movie">Ajouter un film</a>
-    <form action="update_movie" method="post">
+<div class="container">
+    <h1>Administration du site</h1>
+    <div class= "linkContainer">
+        <a href="add_movie"  class="linkToAdmin">>Ajouter un film</a>
+        <form action="update_movie" method="post">
         <select name="id_movie_update" id="id_movie_update">
             <option value=""></option>
             <?php
@@ -12,18 +14,19 @@
             ?>
         </select>
         <input type="submit" value="Modifier ce film"></input>
-    </form>
-    <form action="delete_movie" method="post">
-        <select name="id_movie_delete" id="id_movie_delete">
-            <option value=""></option>
-            <?php
-            foreach ($movies as $movie) {
-            ?>
-                <option value="<?= $movie->id ?>"><?= $movie->name ?></option>
-            <?php
+        </form>
+        <form action="delete_movie" method="post">
+            <select name="id_movie_delete" id="id_movie_delete">
+                <option value=""></option>
+                <?php
+                foreach ($movies as $movie) {
+                ?>
+                    <option value="<?= $movie->id ?>"><?= $movie->name ?></option>
+                <?php
                 }
-            ?>
-        </select>
-        <input type="submit" value="Supprimer ce film"></input>
-    </form>
+                ?>
+            </select>
+            <input type="submit" value="Supprimer ce film"></input>
+        </form>
+    </div>
 </div>
